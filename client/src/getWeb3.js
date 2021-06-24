30a6bd748b53ea6015a0bfcb24b8ff72fc6033f7
Kuan <kuanjh123@gmail.com>
2021-06-23 22:58:57 -0500

    Update getWeb3.js

--- client/src/getWeb3.js
+++ client/src/getWeb3.js
@@ -23,7 +23,7 @@ const getWeb3 = async () => {
     const provider = new Web3.providers.HttpProvider("http://127.0.0.1:8545");
     const web3 = new Web3(provider);
     console.log("No web3 instance injected, using Local web3.");
-    resolve(web3);
+    return web3;
   }
 };
 
